1e7d98d3ce6c2610b353f5074eeceefea68aa3c4
teddyzxcv <pan2691712848@gmail.com>
2021-04-10 13:01:43 +0300

    Create table

--- Program.cs
+++ Program.cs
@@ -14,21 +14,28 @@ namespace Http
         private const string ConnectionString = "Host=139.28.223.173;Port=5432;User ID=aaandreev_4;Password=aaandreev_4;Database=aaandreev_4_db;";
         static async Task Main(string[] args)
         {
-            HttpClient client = new HttpClient();
-            var response = await client.GetAsync("https://api.ratesapi.io/api/latest?base=RUB");
-            var content = await response.Content.ReadAsStringAsync();
-            Console.WriteLine(content);
-            Exchange ex = JsonConvert.DeserializeObject<Exchange>(content);
-            Console.WriteLine(JsonConvert.SerializeObject(ex));
+            // List<Exchange> list = new List<Exchange>();
+            // using var db = new NpgsqlConnection(ConnectionString);
+            // for (DateTime i = new DateTime(2020, 1, 1); i <= DateTime.Now; i = i.AddDays(1))
+            // {
+            //     string sth = i.ToString().Split(' ')[0].Replace("/", "-");
+            //     HttpClient client = new HttpClient();
+            //     var response = await client.GetAsync($"https://api.ratesapi.io/api/{sth}?base=RUB");
+            //     var content = await response.Content.ReadAsStringAsync();
+            //     Exchange ex = JsonConvert.DeserializeObject<Exchange>(content);
+            //     list.Add(ex);
+            // }
+            // db.ExecuteAsync(
+            //         @"insert crm.EXCHANGE(Name) values (@name)",
+            //         new { name });
             await CreateTable();
-
         }
         static async Task CreateTable()
         {
             using var db = new NpgsqlConnection(ConnectionString);
             var query =
                 @"CREATE TABLE crm.EXCHANGE(
-                   ID INT PRIMARY KEY     NOT NULL,
+                   ID SERIAL PRIMARY KEY     NOT NULL,
                    DATA           TEXT    NOT NULL,
                    TO_USD            REAL     NOT NULL,
                    TO_JPY        REAL,
