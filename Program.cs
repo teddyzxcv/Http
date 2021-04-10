5e1a99031608260a3ea18ba74ed4647e8bb67263
teddyzxcv <pan2691712848@gmail.com>
2021-04-10 12:22:57 +0300

    lib

--- Program.cs
+++ Program.cs
@@ -3,10 +3,15 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 using System.Collections.Generic;
+using System.Data;
+using Dapper;
+using Npgsql;
+
 namespace Http
 {
     class Program
     {
+        private const string ConnectionString = "Host=139.28.223.173;Port=5432;User ID=aaandreev_4;Password=aaandreev_4;Database=aaandreev_4_db;";
         static async Task Main(string[] args)
         {
             HttpClient client = new HttpClient();
@@ -16,6 +21,23 @@ namespace Http
             Exchange ex = JsonConvert.DeserializeObject<Exchange>(content);
             Console.WriteLine(JsonConvert.SerializeObject(ex));
 
+        }
+        static async Task CreateTable()
+        {
+            using var db = new NpgsqlConnection(ConnectionString);
+            var query =
+                @"CREATE TABLE crm.COMPANY(
+                   ID INT PRIMARY KEY     NOT NULL,
+                   NAME           TEXT    NOT NULL,
+                   AGE            INT     NOT NULL,
+                   ADDRESS        CHAR(50),
+                   SALARY         REAL
+                );";
+
+            await db.QueryAsync(
+                new CommandDefinition(
+                    query
+                ));
         }
     }
     class Exchange
