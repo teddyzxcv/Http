bdc0da3e9fbfc50f09c9337c6e2abbea2684ef96
teddyzxcv <pan2691712848@gmail.com>
2021-04-10 12:02:08 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Serialization;
+
+namespace Http
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            HttpClient client = new HttpClient();
+            var response = await client.GetAsync("https://api.ratesapi.io/api/latest?base=USD");
+            var content = await response.Content.ReadAsStringAsync();
+            Console.WriteLine(content);
+
+        }
+    }
+}
